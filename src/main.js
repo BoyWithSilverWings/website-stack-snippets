23803941be580b5081f89f8bbc7c79f84c6c2e1c
BoyWithSilverWings <agney@outlook.in>
2018-12-16 19:31:09 +0530

    add vue modal

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
+import VModal from "vue-js-modal";
 
 import router from './router';
 import App from "./App.vue";
@@ -8,6 +9,7 @@ import App from "./App.vue";
 Vue.config.productionTip = false;
 
 Vue.use(VueRouter);
+Vue.use(VModal);
 
 new Vue({
   router,
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -74,7 +74,7 @@ export default {
 
           ${this.html}
 
-      <!-- end snippet -->`
+      <!-- end snippet -->`;
     }
   }
 }
