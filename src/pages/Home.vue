1332e23c0bdbcc44821b6da59662fc15570fdd03
BoyWithSilverWings <agney@outlook.in>
2018-12-17 12:18:44 +0530

    create homepage

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,7 +1,18 @@
 <template>
   <main>
-    <p>Home</p>
-    <router-link to="/templates/create">Create New</router-link>
+    <section class="hero is-medium is-dark is-bold">
+      <div class="hero-body body-container">
+        <div class="container">
+          <h1 class="title">
+            Stack Snippets
+          </h1>
+          <h2 class="subtitle">
+            Create Stackoverflow Snippets easily
+          </h2>
+        </div>
+        <router-link to="/templates/create" class="button is-primary is-rounded is-medium is-outlined">Create</router-link>
+      </div>
+    </section>
   </main>
 </template>
 
@@ -10,3 +21,13 @@ export default {
   name: 'home'
 }
 </script>
+
+<style scoped>
+  .body-container {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: 0 5rem;
+  }
+</style>
+
